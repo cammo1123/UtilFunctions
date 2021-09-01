b1eee73f3a94d81e02aeae317fa6126c93565756
Cameron Youell <cameronyouell@gmail.com>
2021-09-01 12:03:48 +1000

    Added Volume Support

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <Windows.h>
 #include <iostream>
+#include <Mmdeviceapi.h>
+#include <Endpointvolume.h>
+
 int strcmp(char *s1, char *s2)
 {
 	for (; *s1 && *s2; s1++, s2++)
@@ -9,6 +12,34 @@ int strcmp(char *s1, char *s2)
 			return res;
 	}
 	return *s1 - *s2;
+}
+
+int SetVolume(char *nVolume = NULL)
+{
+
+	CoInitialize(NULL);
+
+	IMMDeviceEnumerator *deviceEnumerator = NULL;
+	CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), (LPVOID *)&deviceEnumerator);
+
+	IMMDevice *defaultDevice = NULL;
+	deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &defaultDevice);
+	deviceEnumerator->Release();
+	deviceEnumerator = NULL;
+
+	IAudioEndpointVolume *endpointVolume = NULL;
+	defaultDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_INPROC_SERVER, NULL, (LPVOID *)&endpointVolume);
+	defaultDevice->Release();
+
+	if (nVolume != NULL)
+		endpointVolume->SetMasterVolumeLevelScalar(std::stof(nVolume) / 100, NULL);
+
+	float Volume = 0.0;
+	endpointVolume->GetMasterVolumeLevelScalar(&Volume);
+	endpointVolume->Release();
+
+	CoUninitialize();
+	return Volume * 100.0;
 }
 
 void SendKey(BYTE virtualKey)
@@ -38,5 +69,10 @@ int main(int argc, char *argv[])
 			SendKey(VK_MEDIA_STOP);
 		}
 	}
+	else if (strcmp(argv[1], (char *)"--volume") == 0)
+	{
+		std::cout << SetVolume(argv[2]);
+	}
+
 	return 0;
 }
